51e01258fd0f448132a5067666cdbc9c9e47d545
Boris Krit <scriptboris@gmail.com>
2019-11-09 15:54:30 +0800

    Код для HomeVm

---
+++ SampleXamarin/SampleXamarin/Models/Track.cs
@@ -0,0 +1,17 @@
+﻿using SampleXamarin.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleXamarin.Models
+{
+    public class Track : BaseNotify
+    {
+
+
+        public int Position { get; set; }
+        public string Path { get; set; }
+        public string Artist { get; set; }
+        public string Name { get; set; }
+    }
+}
--- SampleXamarin/SampleXamarin/ViewModels/HomeVm.cs
+++ SampleXamarin/SampleXamarin/ViewModels/HomeVm.cs
@@ -1,13 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using SampleXamarin.Core;
+using SampleXamarin.Models;
 using Xamarin.Forms;
 
 namespace SampleXamarin.ViewModels
 {
     public class HomeVm:BaseViewModel
     {
+        private Track selectedTrack;
+        
+        public HomeVm()
+        {
+            CommandAdd = new LockCommand(this, ActionAdd);
+        }
+
+        public Track SelectedTrack { 
+            get => selectedTrack;
+            set {  
+                selectedTrack = value;
+                ActionSelectTrack(value);
+            } 
+        }
+
+        public LockCommand CommandAdd { get; set; }
+        public LockCommand CommandSelectTrack { get; set; }
+        public ObservableCollection<Track> Tracks { get; set; } = new ObservableCollection<Track>();
+
         public override Page View { get; set; } = new Views.Home();
+
+        private void ActionAdd()
+        {
+            var item = new Track
+            {
+                Position = Tracks.Count + 1,
+                Artist = "FACE",
+                Name = "Я роняю запад",
+                Path = "C:/Musics/MyMusics",
+            };
+            Tracks.Add(item);
+        }
+
+        private void ActionSelectTrack(object obj)
+        {
+            if (obj is Track track)
+            {
+
+            }
+        }
     }
 }
