28e1b9e8093d8dec0e6baa439aad9b0fa5535549
Boris Krit <scriptboris@gmail.com>
2019-11-09 14:05:15 +0800

    Добавьте файлы проекта.

---
+++ SampleXamarin/SampleXamarin/Core/BaseNotify.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SampleXamarin.Core
+{
+    public abstract class BaseNotify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(object obj, string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(obj, new PropertyChangedEventArgs(propName) );
+        }
+
+        public void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ SampleXamarin/SampleXamarin/ViewModels/MainVm.cs
@@ -0,0 +1,14 @@
+﻿using SampleXamarin.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SampleXamarin.ViewModels
+{
+    public class MainVm : BaseViewModel
+    {
+        public override Page View { get; set; } = new Views.MainPage();
+
+    }
+}
